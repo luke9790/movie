4d706ad96424c1ef953771dd9e9f36f4ddbd55ff
lmasetti <luca9790@gmail.com>
2025-02-20 15:36:19 +0100

    fix env

--- src/app/services/themoviedb.service.ts
+++ src/app/services/themoviedb.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { environment } from '../../environment/environment';
+import { environment } from '../environment/environment';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
